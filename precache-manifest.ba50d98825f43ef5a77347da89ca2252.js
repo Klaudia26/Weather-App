4a003544a06bd1802a75f205738678e28ddd1bdd
Klaudia <klaudia.krzywda@gmail.com>
2019-10-15 21:21:16 +0100

    Updates

---
+++ precache-manifest.ba50d98825f43ef5a77347da89ca2252.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "671403e43e6d0970ae6b7fcf6183fb5a",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "9486370144f06b76ac99",
+    "url": "/Weather-App/static/css/main.c0cc3a46.chunk.css"
+  },
+  {
+    "revision": "1b66c4f4fd388e808a22",
+    "url": "/Weather-App/static/js/2.f1e3fa0e.chunk.js"
+  },
+  {
+    "revision": "9486370144f06b76ac99",
+    "url": "/Weather-App/static/js/main.7b8411f4.chunk.js"
+  },
+  {
+    "revision": "3716d3cddd34db689958",
+    "url": "/Weather-App/static/js/runtime-main.81997342.js"
+  },
+  {
+    "revision": "91042db701f25eec97a906be65315add",
+    "url": "/Weather-App/static/media/sunrise.91042db7.jpg"
+  }
+]);
